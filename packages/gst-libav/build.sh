d940c673c24b3a3f192da985656d565afc66950b
Crauzer58 <133880862+Crauzer58@users.noreply.github.com>
2025-02-03 13:28:26 +0000

    Test

---
+++ packages/gst-libav/build.sh
@@ -0,0 +1,14 @@
+PKG_NAME=gst-libav
+PKG_VER=1.24.11
+SRC_URL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-$PKG_VER.tar.xz
+PKG_DEPENDS="gstreamer ffmpeg"
+MESON_ARGS="-Dintrospection=disabled -Dcheck=disabled -Dtests=disabled -Dexamples=disabled"
+MESON_ARGS+="-Dbenchmarks=disabled -Dlibunwind=disabled -Dlibdw=disabled -Dnls=disabled"
+CFLAGS="-I$PREFIX/include"
+LDFLAGS="-L$PREFIX/lib"
+
+build_package() {
+    meson setup build $MESON_ARGS
+    ninja -C build
+    ninja -C build install
+     }
